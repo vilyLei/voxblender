df728c498e6db8b5c5ee449998fb0b43fee89f07
vilyLei <vily313@126.com>
2023-09-06 20:26:35 +0800

    fix: add a few codes.

---
+++ pysrc/base/test01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+abc = 11
+ab = 12
+ac = abc + ab
+print("ac = ",ac)
+
+def add(a,b):
+    return a + b
+
+ac3 = add(13, 15)
+print("ac3 = ",ac3)
+def add1(a, b):
+    return a * a + b + 1
+
+ac4 = add1(13, 15)
+print("ac4 = ",ac4)
+ac4 = add1(3, 5)
+print("ac4 = ",ac4)
+
+print("hi, Geilo, ...")
